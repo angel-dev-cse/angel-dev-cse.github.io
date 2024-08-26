1ede6384e7b15a3290e9a5631a1f4afd19d7ac18
Angel Sharma <angel.dev.cse@gmail.com>
2024-08-27 04:20:16 +0600

    Added svg computer + minor fixes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,23 +3,23 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div id="hero" className="flex flex-col p-5">
+    <div id="hero" className="flex flex-col p-5 relative">
       <div className="flex items-center justify-center sm:items-start sm:justify-start">
         <Image
           src="/assets/images/white.png"
-          height={50}
-          width={50}
+          height={80}
+          width={80}
           alt="White Image"
           className="rounded-full aspect-square object-cover opacity-90 relative"
         />
         <h1
-          className={`${poppinsBold.className} absolute text-center text-2xl text-black-100 ml-0 mt-0 sm:ml-2 sm:mt-2`}
+          className={`${poppinsBold.className} absolute text-center text-4xl text-black-100 ml-0 mt-0 sm:ml-4 sm:mt-5`}
         >
           Hi!
         </h1>
       </div>
 
-      <span className="text-sm text-white-200 mt-5">I am</span>
+      <span className="text-sm text-white-200 mt-10">I am</span>
 
       <div className="align-middle">
         <span className="relative text-3xl mr-2">Angel Sharma</span>
@@ -31,27 +31,45 @@ export default function Hero() {
           className="absolute inline-flex"
         />
       </div>
-      <div className="mt-5 sm:mt-32">
+      <div className="mt-16 sm:mt-25">
         <div className="mb-3">
-          <span className="text-md text-white-200 mr-3">Full Stack</span>
-          <span className="text-5xl font-bold text-white-100">Developer</span>
+          {/* <span className="text-md text-white-200 mr-3">Full Stack</span> */}
+          <span className="text-5xl font-extrabold text-white-100">
+            Full Stack Developer
+          </span>
         </div>
 
+        {/* 
         <div className="mb-3">
-          <span className="text-5xl font-bold text-white-100">Freelancer</span>
-        </div>
+          <span className="text-4xl text-white-100 mr-3">AI Enthusiast</span>
+          <span className="text-md text-white-200"></span>
+        </div> */}
 
-        <div>
-          <span className="text-5xl font-bold text-white-100 mr-3">AI</span>
-          <span className="text-md text-white-200">Enthusiast</span>
+        {/* <div>
+          <span className="text-4xl text-white-100">Freelancer</span>
+        </div> */}
+        <div className="max-w-[30em] text-balance text-white-100 mt-3">
+          Delivering complete software solutions to tackle real life challenges,
+          designing innovative user experience, aspiring to learn new
+          technlogies.
         </div>
       </div>
-      <div className="flex justify-end mt-[-5em] sm:mt-[-10em]">
+      <div className="hidden flex justify-end mt-[-6em] sm:mt-[-16em]">
         <Image
           src="/assets/images/avatar.png"
           width={200}
           height={500}
           alt="White Image"
+          className="ml-10 opacity-5"
+        />
+      </div>
+
+      <div className="absolute top-5 left-[45em] w-full h-full pointer-events-none justify-center">
+        <Image
+          src="/assets/svg/computer.svg"
+          width={400}
+          height={400}
+          alt="Computer SVG"
           className="ml-10"
         />
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,16 @@ import Hero from "@/app/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-0 sm:px-5">
+    <main
+      className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-0 sm:px-20"
+      style={{
+        backgroundImage: "url('/assets/images/background.png')",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPositionY: "top",
+        minHeight: "100vh",
+      }}
+    >
       <div className="max-w-full w-full">
         <Hero />
       </div>
