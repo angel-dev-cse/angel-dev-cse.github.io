92e54e1977cd10059565c5d51d590d2964d4b930
Angel Sharma <angel.dev.cse@gmail.com>
2024-08-26 17:59:38 +0600

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  basePath: "/angel-dev-cse.github.io",
-  assetPrefix: "/angel-dev-cse.github.io/",
   output: "export",
 };
 
